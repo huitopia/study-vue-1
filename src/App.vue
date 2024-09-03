f36442e5083a77a9140ab433d1d841cf78dea335
huitopia <huitopia27@gmail.com>
2024-09-04 00:25:52 +0900

    v-html : 속성의 값으로 데이터를 넣으면 해당 부분이 html 문법으로 출력됨

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,14 @@
   <h1
       v-once
       @click="add">{{ msg }}</h1>
+  <h1 v-html="msg"></h1>
 </template>
 
 <script>
   export default {
     data(){
       return{
-        msg: "hello world!"
+        msg: "<div style='color: blue'>Hello!!</div>"
       }
     },
     methods:{
