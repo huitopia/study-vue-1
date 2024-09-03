b5b987a3b01672a038ccd40299f40eeb893ba5d3
huitopia <huitopia27@gmail.com>
2024-09-03 17:54:40 +0900

    예제 : v-for

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <h1 @click="increase">{{ count }}</h1>
   <div v-if="count > 4">4보다 커야 보임</div>
+  <ul>
+    <li v-for="fruit in fruits"
+        :key="fruit">
+      {{ fruit }}
+    </li>
+  </ul>
 </template>
 <script>
 export default {
   data() {
     return {
-      count: 0
+      count: 0,
+      fruits: ["apple", "banana", "cherry"]
     }
   },
   methods: {
@@ -20,5 +27,9 @@ export default {
 h1 {
   font-size: 30px;
   color: royalblue;
+}
+
+ul li {
+  font-size: 40px;
 }
 </style>
