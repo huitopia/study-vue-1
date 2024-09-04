108dc4a3a9d262ea49c1e4811ce9956cc6b48efe
huitopia <huitopia27@gmail.com>
2024-09-04 11:42:56 +0900

    computed getter & setter getter와 setter를 내부에 재정의해야하기 때문에 메소드 형식이 아닌 객체 형식으로 등록 Getter : ReadOnly Setter : 매개변수를 받아 값 재설정

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
+  <button @click="add">ADD</button>
   <h1>{{ reverseMessage }}</h1>
 </template>
 <script>
 export default {
   data() {
     return {
+      // Getter & Setter
       msg: "hello computed"
     }
   },
   computed: {
-    reverseMessage() {
-      return this.msg.split('').reverse().join('')
+    // Getter
+    // reverseMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    // Getter & Setter
+    reverseMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      set(newMsg) {
+        this.msg = newMsg
+      }
+    }
+  },
+  methods: {
+    add() {
+      this.reverseMessage += "!?"
     }
   }
 }
