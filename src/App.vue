8cdc98fb9aea457063832a84b0b173c4c16673ae
huitopia <huitopia27@gmail.com>
2024-09-04 13:40:27 +0900

    v-bind:class

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1
+      @click="activate"
+      :class="{active: isActive}"
+  >Hello({{ isActive }})</h1>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    activate() {
+      this.isActive = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  color: red;
+}
+</style>
