b7ab87f4dfb67bae3c41ac20d349291745cffe2d
huitopia <huitopia27@gmail.com>
2024-09-04 11:28:24 +0900

    method 대신 computed 사용 이유 : 캐싱 기능으로 최초 연산 시 재연산 안함

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-
+  <h1>{{ reverseMessage }}</h1>
 </template>
 <script>
-
+export default {
+  data() {
+    return {
+      msg: "hello computed"
+    }
+  },
+  computed: {
+    reverseMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  }
+}
 </script>
 <style></style>
